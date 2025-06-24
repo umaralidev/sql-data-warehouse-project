737cb393eb9f1a78594731528447f079b162e5ce
Umar A. <88187350+umaralidev@users.noreply.github.com>
2025-06-25 00:14:38 +0500

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,17 @@
+-- Create Database 'DataWarehouse'
+
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+-- Create database Schemas.
+CREATE SCHEMA bronze;
+GO
+  
+CREATE SCHEMA silver;
+GO
+  
+CREATE SCHEMA gold;
+GO
